77fda58eda68207b759d637d6f21b820220773ef
Ahmed <ahmeddegla@icloud.com>
2024-05-12 17:13:25 +0300

    new 1

--- lib/main.dart
+++ lib/main.dart
@@ -6,13 +6,14 @@ void main() {
   runApp(const MyApp());
 }
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({super.key}); 
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       navigatorKey: RouteUtils.navigatorKey,
+      home: HomeScreen(),
     );
   }
 }
