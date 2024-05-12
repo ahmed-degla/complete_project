70b8f8d9765d2adb5ff6c93d40a1763885de218f
Ahmed <ahmeddegla@icloud.com>
2024-05-12 17:14:04 +0300

    new 2

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       navigatorKey: RouteUtils.navigatorKey,
-      home: HomeScreen(),
     );
   }
 }
